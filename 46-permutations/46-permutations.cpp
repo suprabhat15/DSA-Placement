4eb2648cf91b755da400b7877e60a38dd13fe639
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-05-26 22:27:26 +0530

    Time: 11 ms (12.29%), Space: 8 MB (28.48%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,3 +1,5 @@
+/*
+Approach 1
 class Solution {
 public:
     void helper(vector<int> nums, vector<vector<int>> &ans, vector<int> helperAns, int freq[]){
@@ -22,6 +24,30 @@ public:
         int freq[nums.size()];
         for(int i=0;i<nums.size();i++)freq[i]=0;
         helper(nums, ans, helperAns, freq);
+        return ans;
+    }
+};
+*/
+
+/*Approach 2*/
+class Solution {
+public:
+    void helper(vector<int> nums, int idx, vector<vector<int>> &ans){
+        if(idx == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[idx], nums[i]);
+            helper(nums, idx+1, ans);
+            swap(nums[idx], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+         vector<int> helperAns;
+        vector<vector<int>> ans;
+        helper(nums, 0, ans);
         return ans;
     }
 };
