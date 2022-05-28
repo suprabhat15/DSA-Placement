5516f28d27c9ce01c47b9646cf3bf1860e10e324
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-05-29 00:23:41 +0530

    Update 51-n-queens.cpp

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Method 1
+    class Solution {
 public:
     bool isSafe(int row, int col, int n, vector<string> board){
         // upper(row--, col)
@@ -47,6 +48,43 @@ public:
             board.push_back(s);
         }
         solve(0, n, ans, board);
+        return ans;
+    }
+};
+
+
+// Method 2
+class Solution {
+public:
+    void solve(int col, int n, vector<string> board, vector<vector<string>> &ans, vector<int> lowerDiagonal, vector<int> upperDiagonal, vector<int> horizontalRow) {
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row=0; row<n; ++row){
+            if(lowerDiagonal[row+col] == 0 && upperDiagonal[n-1+col-row] == 0 && horizontalRow[row] == 0){
+               lowerDiagonal[row+col]=1;
+               upperDiagonal[n-1+col-row]=1;
+               horizontalRow[row]=1;
+               board[row][col]='Q';
+               solve(col+1, n, board, ans, lowerDiagonal, upperDiagonal, horizontalRow);
+               lowerDiagonal[row+col]=0;
+               upperDiagonal[n-1+col-row]=0;
+               horizontalRow[row]=0;
+               board[row][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board;
+        string s(n, '.');
+        for(int i=0; i<n;i++){
+            board.push_back(s);
+        }
+        vector<int> lowerDiagonal(2*n-1, 0), upperDiagonal(2*n-1, 0), horizontalRow(n, 0); // method using hashmap
+        solve(0, n, board, ans, lowerDiagonal, upperDiagonal, horizontalRow);
         return ans;
     }
 };
