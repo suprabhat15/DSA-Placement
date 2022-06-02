c077b298ec35e2a78c5e0e162b2a89422eea3eea
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-02 20:50:10 +0530

    Update Maximum-sum-of-non-adjacent-elements.cpp

--- Maximum-sum-of-non-adjacent-elements.cpp
+++ Maximum-sum-of-non-adjacent-elements.cpp
@@ -22,3 +22,23 @@ int maximumNonAdjacentSum(vector<int> &nums){
 }
 RECURSION -> TC = O(2^n), SC = O(N);
 RECURSION + MEMOIZATION -> TC = O(N), SC = O(N) + O(N) {one for recursion stack, other for dp array.}
+
+-----------------------------------------------------------------------------------------------------------
+TABULATION
+TC = O(N), SC = O(N) {this is for dp array.}
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n = nums.size();
+    vector<int> dp(n, 0);
+    dp[0]=nums[0];
+    
+    for(int i=1;i<n;i++){
+        int pick = nums[i];
+        if(i>1) pick += dp[i-2];
+        int notPick = dp[i-1];
+        
+        dp[i] = max(pick, notPick);
+    }
+    
+    return dp[n-1];
+}
+-------------------------------------------------------------------------------------------------------------
