61f2cfae9d1686a129f39f4bf2321a3c27310a97
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-02 21:32:57 +0530

    Time: 3 ms (50.43%), Space: 7.8 MB (25.88%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,4 +1,5 @@
-class Solution {
+/*
+  class Solution {
 public:
     int solve(int idx, vector<int> nums, vector<int>& dp){
         if(idx == 0) return nums[idx];
@@ -18,5 +19,28 @@ public:
         solve(n-1, nums, dp);
         
         return dp[n-1];  // dp[4] means max money which can be robbed from [0,...,4] houses.
+    }
+};
+*/
+
+class Solution {
+public:
+     // Using Tabulation method
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        
+        vector<int> dp(n, 0);
+        dp[0]=nums[0];
+        dp[1]= max(nums[0], nums[1]);
+        
+        for(int i=2;i<n;i++){
+            int pick = nums[i] + dp[i-2];
+            int notPick = dp[i-1];
+            
+            dp[i] = max(pick, notPick);
+        }
+        
+        return dp[n-1];
     }
 };
