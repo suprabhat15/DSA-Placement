3ceab563e6e6fcb43f87b9e27dae5142ebbb7c09
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-02 20:33:25 +0530

    Create Maximum-sum-of-non-adjacent-elements.cpp

---
+++ Maximum-sum-of-non-adjacent-elements.cpp
@@ -0,0 +1,24 @@
+https://www.codingninjas.com/codestudio/problems/maximum-sum-of-non-adjacent-elements_843261?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+You are given an array/list of ‘N’ integers. You are supposed to return the maximum sum of the subsequence with the constraint that 
+no two elements are adjacent in the given array/list.
+--------------------------------------------------------------------------------------
+int solve(int idx, vector<int> nums, vector<int>& dp){
+    if(idx == 0){
+        return nums[0];
+    }
+    if(idx < 0) return 0;
+    if(dp[idx] != -1) return dp[idx];
+    
+    int pick = nums[idx] + solve(idx-2, nums, dp);
+    int notPick = solve(idx-1, nums, dp);
+    
+    return dp[idx] = max(pick, notPick);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return solve(n-1, nums, dp);
+}
+RECURSION -> TC = O(2^n), SC = O(N);
+RECURSION + MEMOIZATION -> TC = O(N), SC = O(N) + O(N) {one for recursion stack, other for dp array.}
