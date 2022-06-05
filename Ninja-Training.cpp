cdfc8f9177528c392fece0bf9f759ccf34ceeba6
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-06 00:51:10 +0530

    Update Ninja-Training.cpp

--- Ninja-Training.cpp
+++ Ninja-Training.cpp
@@ -31,3 +31,27 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     vector<vector<int>> dp(n, vector<int> (4, -1));
     return solve(n-1, 3, points, dp);
 }
+
+----------------------------------------------------------------------------------------------------------------------------
+TC - O(N);
+SC - O(N*4);
+  int ninjaTraining(int n, vector<vector<int>> &points)
+{
+   vector<vector<int>> dp(n, vector<int> (3, 0));
+    
+    dp[0][0] = points[0][0];  // dp[i][j] denotes maxPoints gained by ninja till i days where last performed task is jth index on same day.
+    dp[0][1] = points[0][1];
+    dp[0][2] = points[0][2];
+    
+    int maxPoints = max(dp[0][0], max(dp[0][1], dp[0][2]));
+    
+    for(int i=1;i<points.size();i++){
+        dp[i][0] = points[i][0] + max(dp[i-1][1], dp[i-1][2]);
+        dp[i][1] = points[i][1] + max(dp[i-1][0], dp[i-1][2]);
+        dp[i][2] = points[i][2] + max(dp[i-1][0], dp[i-1][1]);
+        
+        maxPoints = max(dp[i][0], max(dp[i][1], dp[i][2]));
+    }
+    
+    return maxPoints;
+}
