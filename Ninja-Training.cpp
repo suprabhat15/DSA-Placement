066c77a290c8046cb688356ea57817962e8862c0
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-05 23:45:56 +0530

    Update Ninja-Training.cpp

--- Ninja-Training.cpp
+++ Ninja-Training.cpp
@@ -1,7 +1,8 @@
 https://www.codingninjas.com/codestudio/problems/ninja-s-training_3621003?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 TC - O(N*4)*3
-SC - O(N) + O(N*4);
+SC - O(N) + O(N*4); 
+solve(i,j) means , maximum points till day i when previously task j was done.
   int solve(int day, int prev, vector<vector<int>> &points, vector<vector<int>>& dp){  // I had forgot to add '&' before 'points'. So, I was getting TLE.
     if(day == 0){
         int maxi = 0;
