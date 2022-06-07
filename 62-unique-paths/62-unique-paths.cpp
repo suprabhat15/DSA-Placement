13b4f2e4e766254d5684ecca4d03eaa8ff4824e0
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-07 23:21:20 +0530

    Update 62-unique-paths.cpp

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     
-    int solve(int row, int col, vector<vector<int>> &dp){
+int solve(int row, int col, vector<vector<int>> &dp){
     if(row < 0 || col < 0) return 0;
     if(row == 0 && col == 0) return 1;
+
     if(dp[row][col] != -1) return dp[row][col];
+
     int up = solve(row-1, col, dp);
     int left = solve(row, col-1, dp);
     return dp[row][col] = (up + left);
 }
 
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n-1, m-1, dp);
-    }
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    return solve(n-1, m-1, dp);
+  }
 };
