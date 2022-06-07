f8c2b6fb6f8296a5c812f0a15fe961e1a278a35e
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-07 21:29:27 +0530

    Time: 22 ms (6.80%), Space: 13.6 MB (95.28%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& arr) {
-        int n=arr.size();
-        int mx=arr[0];
-        int mn=arr[0];
-        int res=arr[0];
-        for(int i=1;i<n;i++){
-            int v1 = arr[i]*mx;
-            int v2 = arr[i]*mn;
-            mx=max(arr[i],max(v1,v2));
-            mn=min(arr[i],min(v1,v2));
-            // cout<<mx<<" "<<mn<<endl;
-            res=max(res,mx);
-        }
-        return res;        
+int maxProduct(vector<int>& nums) {
+// declared ans to store the final max value till the iteraterd element of the array
+int ans = nums[0];
+// n is the size of the array
+int n = nums.size();
+// imax is the maximum product value till the iteraterd element of the array
+int imax = ans;
+
+    // imin is the minimum product value till the iteraterd element of the array
+    int imin = ans;
+    
+    for(int i=1;i<n;i++) {
+        // If the array element is negative then is multiplied with the max product till now change it to minimum value and min product valu so far to maximum value. Thus swap the max and min value so far 
+        if(nums[i]<0)
+            swap(imax,imin);
+        imax = max(nums[i], imax*nums[i]);
+        imin = min(nums[i], imin*nums[i]);
+        ans = max(ans, imax);
     }
-   };
+    // return the answer
+    return ans;
+}
+};
+// Time Complexity: O(n)
+// Auxiliary Space: O(1)
