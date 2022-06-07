b87dd7aa4aa20021fdb899aff1bf6d209181a946
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-07 19:56:33 +0530

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -16,3 +16,25 @@ int minimumPathSum(vector<vector<int>>& triangle, int n){
     vector<vector<int>> dp(n, vector<int>(n,-1));
 	return solve(0, 0, triangle, n, dp);
 }
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+Tabulation => TC - O(n*n), SC - O(n*n);
+
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	vector<vector<int>> dp(n, vector<int>(n, -1));
+    
+    for(int i=0;i<n;i++){
+        dp[n-1][i] = triangle[n-1][i];
+    }
+    
+    for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int down = triangle[i][j] + dp[i+1][j];
+            int diagonal = triangle[i][j] + dp[i+1][j+1];
+            
+            dp[i][j] = min(down, diagonal);
+        }
+    }
+    
+    return dp[0][0];
+}
