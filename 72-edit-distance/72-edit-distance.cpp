497bc56ff8e3db7c84e79bae7d66276caf6b6cb0
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2022-06-11 23:55:56 +0530

    Time: 177 ms (8.32%), Space: 81.6 MB (5.69%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+     int ed(int i , int j , string word1, string word2,vector<vector<int>>&v){
+        if(i==-1) return j+1;
+        if(j==-1) return i+1;
+      if(v[i][j]!=-1) return v[i][j];
+        if(word1[i]==word2[j]){
+          v[i][j]= ed(i-1,j-1,word1,word2,v); //this is for matching of the last character if they match directly move backward.
+        }
+        else{
+          v[i][j]=1+min(ed(i-1,j-1,word1,word2,v)//this is for the replacement part.
+                    ,min(ed(i-1,j,word1,word2,v)// this is for the deletion part
+                         ,ed(i,j-1,word1,word2,v)));//this is for the insertion
+        }
+        return v[i][j];
+    }
+    int minDistance(string word1, string word2) {
+        vector<vector<int>>v(word1.size()+1,vector<int>(word2.size()+1, -1));
+        
+        return ed(word1.size(),word2.size(),word1,word2,v);
+    }
+};
